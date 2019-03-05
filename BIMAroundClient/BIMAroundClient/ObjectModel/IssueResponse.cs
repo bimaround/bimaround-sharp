f32612e553858295620f43d3c369f12fd1b01dd0
Askhat <asik9810@yandex.ru>
2019-03-05 16:38:54 +0600

    added simple rest client

---
+++ BIMAroundClient/BIMAroundClient/ObjectModel/IssueResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BIMAroundClient.ObjectModel
+{
+    class IssueResponse
+    {
+        public List<Issue> data { get; set; }
+    }
+}
---
+++ BIMAroundClient/BIMAroundClient/ObjectModel/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIMAroundClient.ObjectModel
+{
+    class LoginRequest
+    {
+        public string login { get; set; }//не менять на на заглавную, иначе
+        public string password { get; set; }//в api не парсится json
+    }
+}

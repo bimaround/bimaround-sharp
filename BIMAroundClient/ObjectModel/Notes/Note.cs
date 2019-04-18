05506e7208af0ea229e96d317331858250863c09
Askhat Shaikhin <asik9810@yandex.ru>
2019-04-18 10:19:22 +0600

    added author and createdAt properties for note

--- BIMAroundClient/ObjectModel/Notes/Note.cs
+++ BIMAroundClient/ObjectModel/Notes/Note.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System;
+using BIMAroundClient.ObjectModel.Users;
+
 namespace BIMAroundClient.ObjectModel.Notes
 {
     public class Note
     {
         public string id { get; set; }
         public string text { get; set; }
+        public User author { get; set; }
+        public DateTime createdAt { get; set; }
     }
 }
